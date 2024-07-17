a73337df446c952aa66d4ec46e5a7efa36c7230e
busayo524 <olowookerebusayo66@gmail.com>
2024-07-17 16:40:16 -0700

    task12_advance

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,23 @@
 #include <stddef.h>
 
 /**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
  * struct skiplist_s - singly linked list with an express lane
  *
  * @n: integer
@@ -29,5 +46,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
